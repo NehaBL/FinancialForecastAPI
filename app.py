2505f1487fcdd28ccfc4c70265d06ecae9d43079
NehaBL <blneha2021@gmail.com>
2025-08-05 17:40:40 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,8 @@ app = Flask(__name__)
 
 @app.route('/forecast', methods=['GET'])
 def forecast():
-    return jsonify({'message': 'Financial Forecast API working!'})
+    return jsonify({'message': 'Financial Forecast API is working!'})
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 10000))  # use the port Render provides
-    app.run(host='0.0.0.0', port=port)         # bind to 0.0.0.0
-
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
